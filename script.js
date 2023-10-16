116113f89f1c28d88ece989721b9070c726ad707
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-10-16 22:12:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const evaluatedText = document.getElementById('evaluatedText');
+        const letterCount = document.getElementById('letterCount');
+
+        // Add an input event listener to the text input field
+        evaluatedText.addEventListener('input', updateLetterCount);
+
+        function updateLetterCount() {
+            const text = evaluatedText.value;
+            const count = text.length;
+            letterCount.textContent = count;
+        }
